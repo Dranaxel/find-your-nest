36a5d35a9d074d86a1384601a6cd2263f924ab9a
root <root@ip-10-9-36-26.eu-west-2.compute.internal>
2019-08-05 18:38:17 +0000

    Added login system and configurated redirection

--- FindYourNest/views.py
+++ FindYourNest/views.py
@@ -1,10 +1,14 @@
-from FindYourNest import app, login_manager
+from FindYourNest import app, login_manager 
 from flask import render_template
+from flask_login import login_required
 
 #laoding the login manager
 @login_manager.user_loader
 def load_user(user_id):
     return User.get(user_id)
+
+# allows to redirect the user to the login page if not authentified
+login_manager.login_view ="/connexion"
 
 @app.route("/")
 def main():
@@ -15,9 +19,15 @@ def connexion():
     return render_template("connexion.html")
 
 @app.route("/moncompte")
+@login_required
 def infocompte():
     return render_template("moncompte.html")
 
-@app.route("/Apt/<int: id>")
-def aptInfo()
+@app.route("/Apt/<int:id>")
+def aptInfo():
     return render_template("ficheappart.html")
+
+@app.route("/upAppt/")
+def up_Appt():
+    return render_template("chargementappart.html")
+
