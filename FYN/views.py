197e40ef7a3ab2c576d53e4d83d0e9344d13f9ea
BeatriceBea <52967526+BeatriceBea@users.noreply.github.com>
2019-08-28 15:10:34 +0200

    Ajout return à la fin de la route infoscompte

--- FYN/views.py
+++ FYN/views.py
@@ -235,4 +235,4 @@ def infoscompte():
    return render_template("infoscompte.html", prenom=infos_pro[0], email=infos_pro[1], temps=infos_pro[2], budget=infos_pro[3], type_logement=type_logement, nb=infos_adresse[0], rue=infos_adresse[1], ville=infos_adresse[2], code_postal=infos_adresse[3], titre=infos_favoris[0], prix=infos_favoris[1], photo=infos_favoris[2], description=infos_favoris[3])
 
  else:
-  redirect(url_for('main'))
+  reurn redirect(url_for('main'))
