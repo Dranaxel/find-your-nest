20740d59ccc7ee1efcdec6a18c2c454c5af972e6
BeatriceBea <52967526+BeatriceBea@users.noreply.github.com>
2019-08-29 21:52:55 +0200

    Mise en comm de la partie upload image

--- FYN/__init__.py
+++ FYN/__init__.py
@@ -6,7 +6,7 @@ import sqlite3
 
 app = Flask(__name__)
 app.config.from_object('config.Config')
-app.config['upload_pro'] = upload_pro
+#app.config['upload_pro'] = upload_pro
 
 login_manager = LoginManager()
 login_manager.init_app(app)
