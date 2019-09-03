63c535b4a964cca36a46d4c2ccd420efa07764c6
priya <rajesw.priya@gmail.com>
2019-09-03 14:53:40 +0200

    correction des bugs infoscompte quand favoris inexistant

--- FYN/views.py
+++ FYN/views.py
@@ -206,7 +206,7 @@ def Fiche(id):
 @login_required
 def infoscompte():
     if current_user.is_authenticated:
-        print(current_user.pro)
+        #infospro
         if current_user.pro == 'on':
             email = current_user.id
             infos_pro = c.execute("select prenom, email, temps, budget, maison, appartement, id_utilisateur FROM utilisateur where email=?", (email,)).fetchone()    
@@ -223,11 +223,11 @@ def infoscompte():
 
         else:
             email =  current_user.id
-            infos_pro = c.execute("select prenom, email, temps, budget, maison, appartement, id_utilisateur FROM  utilisateur where email=?", (email,)).fetchone()    
-            maison = infos_pro[4]
-            appartement = infos_pro[5]
+            infos = c.execute("select prenom, email, temps, budget, maison, appartement, id_utilisateur FROM  utilisateur where email=?", (email,)).fetchone()    
+            maison = infos[4]
+            appartement = infos[5]
             infos_adresse = c.execute("select nb, rue, ville, code_postal from adresse inner join utilisateur on adresse.id_adresse=utilisateur.id_adresse where email=?", (email,)).fetchone()
-            id_user=infos_pro[6]
+            id_user=infos[6]
             infos_favoris = c.execute("SELECT titre, prix, photo, description from logement inner join favoris on logement.id_logement=favoris.id_logement where favoris.id_utilisateur=?", (id_user,)).fetchone()
             if maison == 'on':
                 type_logement = 'maison'
@@ -235,7 +235,11 @@ def infoscompte():
                 type_logement = 'appartement'
             else:
                 type_logement = 'Non précisé'
-                return render_template("infoscompte.html", prenom=infos_pro[0], email=infos_pro[1], temps=infos_pro[2], budget=infos_pro[3], type_logement=type_logement, nb=infos_adresse[0], rue=infos_adresse[1], ville=infos_adresse[2], code_postal=infos_adresse[3], titre=infos_favoris[0], prix=infos_favoris[1], photo=infos_favoris[2], description=infos_favoris[3])
+
+            if infos_favoris is None : 
+                return render_template("infoscompte.html", prenom=infos[0], email=infos[1], temps=infos[2], budget=infos[3], type_logement=type_logement, nb=infos_adresse[0], rue=infos_adresse[1], ville=infos_adresse[2], code_postal=infos_adresse[3])
+            else:
+                return render_template("infoscompte.html", prenom=infos[0], email=infos[1], temps=infos[2], budget=infos[3], type_logement=type_logement, nb=infos_adresse[0], rue=infos_adresse[1], ville=infos_adresse[2], code_postal=infos_adresse[3], titre=infos_favoris[0], prix=infos_favoris[1], photo=infos_favoris[2], description=infos_favoris[3])
 
     else:
         return redirect(url_for('main'))
