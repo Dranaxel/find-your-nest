1e56b400f7e47bc559dbc93adf7ca3433ef9d4c5
Alexandre <a.marce@free.fr>
2019-09-08 21:05:10 +0000

    added username in git url

--- .push.sh
+++ .push.sh
@@ -12,7 +12,7 @@ commit_website_files() {
 }
 
 upload_files() {
-  git remote add origin https://${GH_TOKEN}@github.com/Dranaxel/find-your-nest.git > /dev/null 2>&1
+  git remote add origin https://Dranaxel:${GH_TOKEN}@github.com/Dranaxel/find-your-nest.git > /dev/null 2>&1
   git push --quiet --set-upstream origin master-1
 }
 
