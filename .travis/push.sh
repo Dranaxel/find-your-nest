174283a1e9a61e8519a1d6ea2b1cee4498857013
Alexandre <a.marce@free.fr>
2019-09-08 20:45:32 +0000

    inserted good repo link

--- .travis/push.sh
+++ .travis/push.sh
@@ -7,12 +7,12 @@ setup_git() {
 
 commit_website_files() {
   git checkout -b master-1 
-  git add .
+  git add . *
   git commit --message "Travis build: $TRAVIS_BUILD_NUMBER"
 }
 
 upload_files() {
-  git remote add origin https://${GH_TOKEN}@github.com/MVSE-outreach/resources.git > /dev/null 2>&1
+  git remote add origin https://${GH_TOKEN}@github.com/Dranaxel/find-your-nest.git > /dev/null 2>&1
   git push --quiet --set-upstream origin master-1
 }
 
