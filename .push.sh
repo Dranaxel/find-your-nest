26236abd6edec66af7eb1b5b2caf7832b78ce68b
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-09-09 11:43:54 +0000

    Deploy Dranaxel/find-your-nest to github.com/Dranaxel/find-your-nest.git:master-1

---
+++ .push.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+
+setup_git() {
+  git config --global user.email "travis@travis-ci.org"
+  git config --global user.name "Travis CI"
+}
+
+commit_website_files() {
+  git checkout -b master-1 
+  git add *
+  git commit --message "Travis build: $TRAVIS_BUILD_NUMBER"
+}
+
+upload_files() {
+  git remote add origin https://Dranaxel:${GH_TOKEN}@github.com/Dranaxel/find-your-nest.git > /dev/null 2>&1
+  git push --quiet --set-upstream origin master-1
+}
+
+setup_git
+commit_website_files
+upload_files
