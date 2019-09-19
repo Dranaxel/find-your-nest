acdf1b5ffff6b44be6e294f0d7d7ec5f502fb3e8
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-09-19 08:19:26 +0000

    Deploy Dranaxel/find-your-nest to github.com/Dranaxel/find-your-nest.git:master-1

--- tests/test_selenium.py
+++ tests/test_selenium.py
@@ -20,7 +20,7 @@ driver = webdriver.Remote(
    command_executor='https://{}:{}@ondemand.eu-central-1.saucelabs.com/wd/hub'.format(username, access_key),
    desired_capabilities=desired_cap)
 
-driver.get("https://flask-ywhzbcfbpq-ue.a.run.app")
+driver.get("https://find-your-nest-ywhzbcfbpq-ew.a.run.app/")
 time.sleep(2)
 driver.find_element(By.LINK_TEXT, "Se connecter").click()
 driver.find_element(By.ID, "exampleInputMail").click()
