cdfb2fd5ceb65919c348f082bc86eefeaead50f7
priya <rajesw.priya@gmail.com>
2019-09-19 16:26:00 +0200

    Création de la forget_password pour le mail de réinitilisation du mail

--- FYN/mail.py
+++ FYN/mail.py
@@ -1,5 +1,6 @@
 from mailjet_rest import Client
 import os
+from flask import url_for
 
 def envoyer_mail(mail,username):
     api_key = '4758ff5a850d727d75cbc8e7bf09c958'
@@ -28,3 +29,33 @@ def envoyer_mail(mail,username):
     result = mailjet.send.create(data=data)
     print(result.status_code)
     print(result.json())
+
+
+def forget_password(mail, username):
+    api_key = '4758ff5a850d727d75cbc8e7bf09c958'
+    api_secret = '50594a1889d64dee6cbf9d98f5ee5c09'
+    mailjet = Client(auth=(api_key, api_secret), version='v3.1')
+    data = {
+    'Messages': [
+        {
+        "From": {
+            "Email": "rajesw.priya@gmail.com",
+            "Name": "Priya"
+        },
+        "To": [
+            {
+            "Email": mail,
+            "Name": username
+            }
+        ],
+        "Subject": "Réinitialiser votre mot de passe FINDYOURNEST",
+        "TextPart": "Réinitialisation",
+        "HTMLPart": "<h3>Bonjour, Veuillez réinitialiser votre mot de passe en cliquant sur le lien suivant <a href='http://127.0.0.1:8080/reinialisation_pwd/'>Mot_de_passe</a>!</h3><br /> Merci, en vous souhaitant une bonne journée"
+        }
+    ]
+    }
+    result = mailjet.send.create(data=data)
+    print(result.status_code)
+    print(result.json())
+
+"<h3> Bonjour, vous nous avez indiqué avoir oublié votre mot de passe. Si c'est vraiment le cas, cliquez sur le lien ci-dessous afin de le réinitialiser: <a href='{{ url_for('reinitialisation_pwd')}}'>Réinitialiser votre mot de passe</a></h3></br>Si vous n'aviez pas l'intention de réinitialiser votre mot de passe ne sera pas changé." 
