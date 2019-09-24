c735bb7dadb6403232aaf6723860f278d3f7180d
Alexandre <a.marce@free.fr>
2019-09-24 09:17:33 +0000

    deleting debug

--- app.py
+++ app.py
@@ -4,4 +4,4 @@ import os
 from FYN import app 
 
 if __name__ == '__main__':
-    app.run(host="0.0.0.0", debug=True, port=int(os.environ.get('PORT', 8080)))
+    app.run(host="0.0.0.0", port=int(os.environ.get('PORT', 8080)))
