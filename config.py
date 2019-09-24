a72c6685d19517f5d2b2ad2050992cc5d099a105
Alexandre <a.marce@free.fr>
2019-09-24 12:23:34 +0000

    modified opencage key

--- config.py
+++ config.py
@@ -5,7 +5,7 @@ class Config(object):
     SECRET_KEY = b"ZLtx0B9Lnsoq32oV_lXG/S[c1}g_vl"
     NAVITIA_KEY = "6451613e-bdfe-4194-a3b5-91164c6ae89f"
     NAVITIA_URL = "http://api.navitia.io/v1/journeys"
-    OPENCAGE_KEY = "3c853893fc37402eb2ef1473b6629218"
+    OPENCAGE_KEY = "31dbec8c7f4844a6aafa18c738aea931"
 
 class DevelopmentConfig(Config):
     DEBUG = False
