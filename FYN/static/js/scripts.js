25fd2bf12de164fc70ffd3c6816644246da2aa3e
prajes <52969089+prajes@users.noreply.github.com>
2019-09-26 16:57:45 +0200

    Ajout du flash quand logement déjà dans favoris

--- FYN/static/js/scripts.js
+++ FYN/static/js/scripts.js
@@ -47,6 +47,55 @@ for (let i = 0; i <= jsBtnFav.length - 1; ++i) {
 }
 
 
+if (document.getElementById("jsBtnFav")) {  //is on result page ?
+
+    let jsBtnFav = document.getElementById("jsBtnFav")
+    let loader = `
+        <div class="spinner-border" role="status">
+            <span class="sr-only">Loading...</span>
+        </div>
+    `
+    let btnText = "Ajouter aux favoris"
+    let isLoading = false;
+
+
+    jsBtnFav.addEventListener('click', (event) => {
+        event.preventDefault();
+        jsBtnFav.innerHTML = loader;
+        jsBtnFav.setAttribute('disabled', "");
+        isLoading = true;
+        let favId = jsBtnFav.getAttribute("data-value");
+        let alertDiv = jsBtnFav.parentNode.parentNode.parentNode.parentNode.parentNode.getElementsByClassName('alertFav')[0];
+console.log(favId)
+        fetch(`http://localhost:8080/getFavorite/${favId}`, {
+            method: "POST"
+        }).then(data => {
+            data.json().then(json => {
+
+                if (isLoading) {
+                    jsBtnFav.innerHTML = btnText;
+                    jsBtnFav.removeAttribute('disabled');
+                }
+
+                if (json.is) {
+                    alertDiv.innerHTML = buildAlert(json.msg, "success")
+                    setTimeout(() => {
+                        alertDiv.innerHTML = ""
+                    }, 2000)
+                } else {
+                    alertDiv.innerHTML = buildAlert(json.msg, "warning")
+                    setTimeout(() => {
+                        alertDiv.innerHTML = ""
+                    }, 2000)
+                }
+            })
+        })
+    })
+}
+
+
+
+
 function buildAlert(msg, type) {
     return `
     <div class="alert alert-${type}" role="alert">
